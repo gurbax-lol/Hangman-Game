598932662f533a0d6dd83aef11907b402b1f1217
Gurbax <122194475+gurbax-lol@users.noreply.github.com>
2023-01-12 12:27:52 +0530

    Add main source code

---
+++ main.py
@@ -0,0 +1,58 @@
+# Hangman 
+# Some source code and art by Angela Yu from London App Brewery, as part of their Python Pro Bootcamp https://www.londonappbrewery.com/
+
+from replit import clear
+import random
+import hangman_words
+import hangman_art
+
+
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+print(hangman_art.logo)
+#Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    clear()
+    if guess in chosen_word:
+      print("You've already guessed that letter.")
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"{guess} is not in the chosen word.")
+        lives -= 1
+        if lives > 1:
+          print(f"Lives left: {lives}")
+        else:
+          print("LAST LIFE!")
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+            print(f"The word was: {chosen_word}")
+
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
